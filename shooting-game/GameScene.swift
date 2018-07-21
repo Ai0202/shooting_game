81c7372cd0b854e03a994b8159dcb88a620d034d
Atsushi <>
2018-07-21 08:49:14 +0800

    デフォルト生成されるいらないコード削除

---
+++ shooting-game/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  shooting-game
+//
+//  Created by Atsushi on 2018/07/21.
+//  Copyright © 2018年 Atsushi. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+}
